a2b4a5fc109f08ff5923d5eebda8602470b7edaf
Batman61x <ercanbaksoy97@gmail.com>
2025-11-27 19:57:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// === SUPABASE SETUP ===
+const supabaseUrl = "https://ymrjacrxdtwnllvybhhcn.supabase.co";
+const supabaseAnonKey = "DEIN_ANON_KEY_HIER_REIN"; // den du kopiert hast
+
+const supabaseClient = supabase.createClient(supabaseUrl, supabaseAnonKey);
 // IntersectionObserver animations
 const io = new IntersectionObserver((entries)=>{
   entries.forEach(e=>{ if(e.isIntersecting){ e.target.classList.add('in'); io.unobserve(e.target); } });
